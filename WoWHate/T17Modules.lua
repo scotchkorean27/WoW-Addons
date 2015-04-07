02eb051206a7f5e9117fc0e04e885328bfe8f0e6
scotchkorean27 <scottlee27@hotmail.com>
2015-04-07 00:19:41 -0400

    Finished T17 Modules and code

---
+++ WoWHate/T17Modules.lua
@@ -0,0 +1,75 @@
+function loadBlackHandModule()
+	modloaded = true;
+	print("Blackhand module loaded");
+	local t = {};
+	t[156096] = "Marked for Death";
+	return t, "Blackhand";
+end;
+function loadMaidsModule()
+	modloaded = true;
+	print("Iron Maidens module loaded");
+	local t = {};
+	t[156631] = "Rapid Fire";
+	t[164271] = "Penetrating Shot";
+	return t, "Iron Maidens";
+end;
+function loadBeastLordModule()
+	modloaded = true;
+	print("Beastlord Darmac module loaded");
+	local t = {};
+	t[154981] = "Conflagration";
+	return t, "Beastlord Darmac";
+end;
+function loadFlameBenderModule()
+	modloaded = true;
+	print("Flamebender Ka'graz module loaded");
+	local t = {};
+	t[154932] = "Molten Torrent";
+	t[154952] = "Fixate";
+	t[155277] = "Blazing Radiance"
+	return t, "Flamebender Ka'graz";
+end;
+function loadBFurnModule()
+	modloaded = true;
+	print("Blast Furnace module loaded");
+	local t = {};
+	t[155192] = "Bomb";
+	t[155196] = "Fixate";
+	t[176121] = "Volatile Fire"
+	return t, "Blast Furnace";
+end;
+function loadGruulModule()
+	modloaded = true;
+	print("Gruul module loaded");
+	local t = {};
+	t[155330] = "Petrify";
+	return t, "Gruul";
+end;
+function loadKargathModule()
+	modloaded = true;
+	print("Kargath module loaded.");
+	local t = {};
+	t[158986] = "Berserker Rush";
+	return t, "Kargath Bladefist";
+end;
+function loadTectusModule()
+	modloaded = true;
+	print("Tectus module loaded.");
+	local t = {};
+	t[162346] = "Crystalline Barrage";
+	return t, "Tectus";
+end;
+function loadTestModule()
+	modloaded = true;
+	print("Test Module Loaded");
+	local t = {};
+	t[157335] = "Will of the Necropolis";
+	return t, "Test";
+end;
+function loadRTestModule()
+	modloaded = true;
+	print("Raid Test Module Loaded");
+	local t = {};
+	t[163241] = "Rot";
+	return t, "RTest";
+end;
--- WoWHate/WowHate.lua
+++ WoWHate/WowHate.lua
@@ -1,5 +1,8 @@
 raidg = {};
 debuffs = {};
+rdbtable = {};
+pasttable = {};
+fname = "";
 incombat = false;
 modloaded = false;
 local frame = CreateFrame("FRAME", "ScottFrame");
@@ -9,35 +12,27 @@ frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 frame2:RegisterEvent("PLAYER_REGEN_DISABLED");
 frame3:RegisterEvent("PLAYER_REGEN_ENABLED");
 
-local function loadKargathModule()
-	modloaded = true;
-	print("Kargath module loaded.");
-	local t = {};
-	t["Berserker Rush"] = 1;
-	return t;
-end;
-local function loadTectusModule()
-	modloaded = true;
-	print("Tectus module loaded.");
-	local t = {};
-	t["Crystalline Barrage"] = 1;
-	return t;
-end;
-local function loadTestModule()
-	modloaded = true;
-	print("Test Module Loaded");
-	local t = {};
-	t["Will of the Necropolis"] = 1;
-	return t;
-end;
 
+
+local function buildRDBTable(raid, debuff)
+	local t = {};
+	for key, value in pairs(raid) do
+		t[key] = {};
+	end;
+	for player, db in pairs(t) do
+		for ID, dname in pairs(debuff) do
+			t[player][dname] = false;
+		end;
+	end;
+	return t;
+end;
 local function makeRaidTable()
 	local gnum = GetNumGroupMembers();
 	local t = {};
 		for i = 1, gnum, 1 do
 		local s = "raid" .. i;
 		local uname = GetUnitName(s, true);
-		print("Adding entry for: " .. uname);
+		--print("Adding entry for: " .. uname);
 		t[uname] = {};
 	end;
 	return t;
@@ -49,50 +44,116 @@ local function makeTestTable()
 	t[uname] = {};
 	return t;
 end;
+local function reportTable(raid, channel)
+	SendChatMessage("WoW Hates Aurorraa report for: " .. fname ,channel);
+	for key, value in pairs(raid) do	
+		SendChatMessage("People targetted by: " .. key, channel);
+		for key2, value2 in pairs(value) do
+			if(value2 > 10) then
+				print(key2 .. ": " .. value2);
+			end;
+		end;
+		for i = 10, 1, -1 do
+			for key2, value2 in pairs(value) do
+				if(value2 == i) then
+					SendChatMessage(key2 .. ": " .. value2, channel);
+				end;
+			end;
+		end
+		print(" ");
+	end;
+end;
 local function printTable(raid, debuff)
+	print("|cff00ffffValues for " .. fname);
 	local invraid = {};
 	for key, value in pairs(debuff) do
-		invraid[key] = {};
+		invraid[value] = {};
 	end;
-	for key, value in pairs(raid) do
-		for key2, value2 in pairs(value) do
-			invraid[key2][key] = value2;
+	for player, pdset in pairs(raid) do
+		for dname, count in pairs(pdset) do
+			invraid[dname][player] = count;
 		end;
 	end;
 	for key, value in pairs(invraid) do	
-		print("People hit by: " .. key);
+		print("|cffff0000People targetted by: " .. key);
 		for key2, value2 in pairs(value) do
-			print(key2 .. ": " .. value2);
+			if(value2 > 10) then
+				print(key2 .. ": " .. value2);
+			end;
 		end;
+		for i = 10, 1, -1 do
+			for key2, value2 in pairs(value) do
+				if(value2 == i) then
+					print(key2 .. ": " .. value2);
+				end;
+			end;
+		end
+		print(" ");
 	end;
+	return invraid;
 end;
 
 local function registerAura(self,event, ...)
 	
 	local timestamp, etype, hideCaster, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags, spellId, spellName, spellSchool, amount, overkill, school, resisted, blocked, absorbed, critical = select(1, ...);
-	if(modloaded == false) then
+	if(modloaded == false and incombat == true) then
 		if(destName == "Kargath Bladefist") then
-			debuffs = loadKargathModule();
+			debuffs, fname = loadKargathModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Blackhand") then
+			debuffs, fname = loadBlackHandModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Marak the Blooded") then
+			debuffs, fname = loadMaidsModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Beastlord Darmac") then
+			debuffs, fname = loadBeastLordModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Flamebender Ka'graz") then
+			debuffs, fname = loadFlameBenderModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Gruul") then
+			debuffs, fname = loadGruulModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
+		elseif(destName == "Foreman Feldspar") then
+			debuffs, fname = loadBFurnModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
 		elseif(destName == "Tectus") then
-			debuffs = loadTectusModule();
+			debuffs, fname = loadTectusModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
 		elseif(destName == "Dungeoneer's Training Dummy") then
-			debuffs = loadTestModule();
+			debuffs, fname = loadTestModule();
+			rdbtable = buildRDBTable(raidg, debuffs);
 		end;
 	end;
-	if(etype == 'SPELL_AURA_APPLIED' and amount == 'DEBUFF' and incombat == true and UnitInRaid(destName)) then
-		if(modloaded == false) then
-			print(destName .. " " .. spellName);
-			debuffs[spellName] = 1;
-			if(raidg[destName][spellName] == nil) then
-				raidg[destName][spellName] = 0;
+	if((etype == 'SPELL_AURA_APPLIED' or etype == 'SPELL_AURA_REMOVED') and amount == 'DEBUFF' and incombat == true and (destName == GetUnitName("player") or UnitInRaid(destName))) then
+		--if(modloaded == false and etype == 'SPELL_AURA_APPLIED') then
+			--print(destName .. " " .. spellName);
+			--local name, rank, icon, count, dispel, duration, expires, caster, steal, console, ID, canapp, boss, v1, v2, v3 = UnitDebuff(destName, spellName);
+			--responsetime = duration + (GetTime() - expires);
+			--print("response time is" .. responsetime);
+			--debuffs[ID] = spellName;
+				--print(name .. ID );
+				--if(raidg[destName][spellName] == nil) then
+					--raidg[destName][spellName] = 0;
+				--end;
+				--raidg[destName][spellName] = raidg[destName][spellName] + 1;
+		if(modloaded == true) then
+			local name, rank, icon, count, dispel, duration, expires, caster, steal, console, ID, canapp, boss, v1, v2, v3 = UnitDebuff(destName, spellName);
+			if(name ~= nil) then
+				print(name .. " " .. ID .. " " ..destName);
 			end;
-			raidg[destName][spellName] = raidg[destName][spellName] + 1;
-		elseif(modloaded == true and debuffs[spellName] ~= nil and UnitInRaid(destName)) then
-			print(destName .. " " .. spellName);
-			if(raidg[destName][spellName] == nil) then
-				raidg[destName][spellName] = 0;
+			if(name ~= nil and debuffs[ID] ~= nil and rdbtable[destName][spellName] == false) then
+				print(destName .. " " .. spellName);
+				if(raidg[destName][spellName] == nil) then
+					raidg[destName][spellName] = 0;
+				end;
+				raidg[destName][spellName] = raidg[destName][spellName] + 1;
+				rdbtable[destName][spellName] = true;
+			elseif(name == nil and rdbtable[destName][spellName] ~= nil and rdbtable[destName][spellName] == true) then
+				rdbtable[destName][spellName] = false;
+				print(spellName .. " has fallen off " .. destName);
 			end;
-			raidg[destName][spellName] = raidg[destName][spellName] + 1;
 		end;
 	end;
 end;
@@ -100,13 +161,20 @@ end;
 local function enterCombat(self, event, ...)
 	print("Generating Table...");
 	incombat = true;
-	raidg = makeRaidTable();
+	if(IsInRaid() == true) then
+		raidg = makeRaidTable();
+	else
+		raidg = makeTestTable();
+	end;
 end;
 
 local function exitCombat(self, event, ...)
-	print("Printing...");
 	incombat = false;
-	printTable(raidg, debuffs);
+	if(modloaded == true) then
+		pasttable = printTable(raidg, debuffs);
+		modloaded = false;
+	end;
+	debuffs = {};
 end;
 
 frame:SetScript("OnEvent", registerAura);
@@ -115,18 +183,26 @@ frame3:SetScript("OnEvent", exitCombat);
 
 
 
-SLASH_LOADMOD1 = '/HateLoad';
+SLASH_LOADMOD1 = '/WHA';
 local function modloader(msg, editbox)
-	if msg == 'Kargath' then
-		debuffs = loadKargathModule();
-	elseif msg == 'Test' then
-		debuffs = loadTestModule();
-	elseif msg == 'Clear' then
-		debuffs = {};
+	if msg == 'grep' then
+		reportTable(pasttable, "GUILD");
+	elseif msg == 'irep' then
+		reportTable(pasttable, "RAID");
+	elseif msg == 'trep' then
+		reportTable(pasttable, "SAY")
+	elseif msg == 'clear' then
 		modloaded = false;
-		print("Cleared module. Entering Blank Slate Mode");
+		raidg = {};
+		debuffs = {};
+		fname = {};
+		pasttable = {};
+		rdbtable = {};
 	else
-		print("Module not found");
+		print("WoW Hates Aurorra options");
+		print("Type /WHA grep to print your report to your guild");
+		print("Type /WHA irep to print your report to your raid");
+		print("Type /WHA clear to clear all values.");
 	end;
 end;
 SlashCmdList["LOADMOD"] = modloader;
